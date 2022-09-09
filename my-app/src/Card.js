e3993ecbd95e1d7fcba75f1350ed6513b7025464
abooBala <aboobala64@gmail.com>
2022-09-09 23:34:40 +0100

    Passing attributes through props

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,23 +1,22 @@
 import './AirbnbExperience.css'
-import CardOne from './images/card-img-1.png'
 import star from './images/Star.png'
 import ellipse from './images/ellipse.png'
 
-export default function Card() {
+export default function Card(props) {
     return(
-        <main>
+        <section>
             <div className='Card'>
-                <img src={CardOne} alt="Life lessons with Katie Zaferes" className="Card--image"/>
+                <img src={props.img} alt="Life lessons with Katie Zaferes" className="Card--image"/>
                 <div className='Card--stats'>
                     <img className='Card--star' src={star} alt="Card Rating" />
-                    <span>5.0 &nbsp;</span>
-                    <span className="gray">(6) <img src={ellipse} />&nbsp;</span>
-                    <span className="gray">USA</span>
+                    <span>{props.rating} &nbsp;</span>
+                    <span className="gray">({props.ratingCount}) <img src={ellipse} />&nbsp;</span>
+                    <span className="gray">{props.location}</span>
                 </div>
-                    <p className="Card--description m-0">Life lessons with Katie Zaferes</p>
-                    <p className="m-0"><b>From $136</b>/ person</p>
+                    <p className="Card--description m-0">{props.name}</p>
+                    <p className="m-0"><b>From ${props.price}</b>/ person</p>
             </div>
 
-        </main>
+        </section>
     )
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,14 +4,45 @@ import './AirbnbExperience.css';
 import AirbnbExperienceNavbar from './AirbnbExperienceNavbar.js';
 import Card from './Card.js'
 import AirbnbExperienceHero from './AirbnbExperienceHero.js'
+import ImgOne from './images/card-img-1.png'
+import ImgTwo from './images/card-img-2.png'
+import ImgThree from './images/card-img-3.png'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(  
+root.render( 
   <>
     <AirbnbExperienceNavbar />
-    <AirbnbExperienceHero />
-    <Card />
-  </>
+    <main>
+      <AirbnbExperienceHero />
+      <div className='Cards'>
+        <Card
+          img={ImgOne}
+          rating="5"
+          ratingCount="5"
+          location="USA"
+          name="Life lessons with Katie Zaferes"
+          price=" 136"        
+        />
+        <Card 
+          img={ImgTwo}
+          rating="5"
+          ratingCount="30"
+          location="USA"
+          name="Learn wedding photography"
+          price=" 125"   
+        />
+        <Card
+          img={ImgThree} 
+           rating="4.8"
+           ratingCount="2"
+           location="USA"
+           name="Group Mountain Biking"
+           price=" 50"         
+        />
+      </div>
+    </main>
+  </> 
 );
 
 // If you want to start measuring performance in your app, pass a function
