483dd83510b28cc3cfc351a8d6252ccba151e00a
abooBala <aboobala64@gmail.com>
2022-09-11 22:25:23 +0100

    Added data files and consumed data

---
+++ my-app/src/data.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        id: 1,
+        imgURL: "images/katie_zaferes.png",
+        title: "Life lessons with Katie Zaferes",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        price: 136,
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: 'USA',
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        imgURL: "images/wedding_photography.png",
+        title: "Learn wedding photography",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        price: 125,
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: 'Online',
+        openSpots: 10,
+    },
+    {
+        id: 3,
+        imgURL: "images/mountain_biking.png",
+        title: "Group Mountain Biking",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        price: 50,
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: 'USA',
+        openSpots: 5,
+    }, 
+    {
+        id: 4,
+        imgURL: "images/aboo_bala.png",
+        title: "React Dev with Aboo Bala",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        price: 350,
+        stats: {
+            rating: 4.7,
+            reviewCount: 234
+        },
+        location: 'NGA',
+        openSpots: 0,
+    }
+]
