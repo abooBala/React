b777d7a2aeaf2d38c50eb8150c8df7b38e37e984
abooBala <aboobala64@gmail.com>
2022-09-11 22:55:15 +0100

    modified component

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -3,20 +3,29 @@ import star from './images/Star.png'
 import ellipse from './images/ellipse.png'
 
 export default function Card(props) {
-    return(
-        <section>
-            <div className='Card'>
-                <img src={props.img} alt="Life lessons with Katie Zaferes" className="Card--image"/>
-                <div className='Card--stats'>
-                    <img className='Card--star' src={star} alt="Card Rating" />
-                    <span>{props.rating} &nbsp;</span>
-                    <span className="gray">({props.ratingCount}) <img src={ellipse} />&nbsp;</span>
-                    <span className="gray">{props.location}</span>
-                </div>
-                    <p className="Card--description m-0">{props.name}</p>
-                    <p className="m-0"><b>From ${props.price}</b>/ person</p>
-            </div>
 
+    let badgeText
+    if (props.openSpots == 0) {
+        badgeText = "SOLD OUT"
+    } 
+    else if (props.location == "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return(
+        <section>            
+            <div className='Card'>
+                {badgeText && <div className='Card--badge'>{badgeText}</div>}
+                <img src={props.item.imgURL} alt="Life lessons with Katie Zaferes" className="Card--image"/>
+                <div className='Card--stats'>
+                    <img className='Card--star pr-2' src={star} alt="Card Rating" />
+                    <span>{props.item.stats.rating} &nbsp;</span>
+                    <span className="gray">({props.item.stats.reviewCount}) <img src={ellipse} />&nbsp;</span>
+                    <span className="gray">{props.item.location}</span>
+                </div>
+                    <p className="Card--description m-0">{props.item.title}</p>
+                    <p className="m-0"><b>From ${props.item.price}</b>/ person</p>
+            </div>
         </section>
     )
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,50 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './AirbnbExperience.css';
 import AirbnbExperienceNavbar from './AirbnbExperienceNavbar.js';
-import Card from './Card.js'
 import AirbnbExperienceHero from './AirbnbExperienceHero.js'
-import ImgOne from './images/card-img-1.png'
-import ImgTwo from './images/card-img-2.png'
-import ImgThree from './images/card-img-3.png'
-
+import data from './data.js'
+import Card from './Card.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const cards = data.map(item => {
+  return (
+    <Card 
+      key={item.id}
+      item={item}
+    />
+  )
+})
+
 root.render( 
   <>
     <AirbnbExperienceNavbar />
     <main>
       <AirbnbExperienceHero />
       <div className='Cards'>
-        <Card
-          img={ImgOne}
-          rating="5"
-          ratingCount="5"
-          location="USA"
-          name="Life lessons with Katie Zaferes"
-          price=" 136"        
-        />
-        <Card 
-          img={ImgTwo}
-          rating="5"
-          ratingCount="30"
-          location="USA"
-          name="Learn wedding photography"
-          price=" 125"   
-        />
-        <Card
-          img={ImgThree} 
-           rating="4.8"
-           ratingCount="2"
-           location="USA"
-           name="Group Mountain Biking"
-           price=" 50"         
-        />
+        {cards}                
       </div>
     </main>
   </> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
