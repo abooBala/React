66c1789ad4608298dbdcbe4d550203d0c942f9a4
abooBala <aboobala64@gmail.com>
2022-09-14 08:11:03 +0100

    Handling count with State

--- my-app/src/LearnStates.js
+++ my-app/src/LearnStates.js
@@ -2,9 +2,18 @@ import React from "react"
 
 export default function LearnStates() {
     const [result, setResult] = React.useState("Yes")
+    const [count, setCount] = React.useState(0)
 
     function handleClick() {
         setResult("No")
+    }
+
+    function addCount() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function minusCount() {
+        setCount(prevCount => prevCount - 1)
     }
 
     return(
@@ -14,6 +23,13 @@ export default function LearnStates() {
                 <button type="button">Find out</button>
                 <p>{result}</p>
             </div>
+
+            <div>
+                <button className="decrease" onClick={minusCount}>Minus</button>
+                <h1>{count}</h1>
+                <button className="increase" onClick={addCount}>Add</button>
+            </div>
+
         </div>
     )
 }
